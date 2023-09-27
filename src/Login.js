e5a566b0b65a95f8e6e1ebafc41f5fea22486f0c
RanjaniAnandan <ranjanianandan2001@gmail.com>
2023-09-27 21:32:13 +0530

    Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,42 @@
-import React from 'react'
-import "./Login.css"
-import { Button } from '@material-ui/core'
-import { auth, provider } from './firebase';
-import { actionTypes } from './reducer';
-import { useStateValue } from './stateProvider';
+import React from "react";
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./stateProvider";
 
 function Login() {
-    const [{}, dispatch] = useStateValue();  
-     const signIn = () => {
-        auth
-        .signInWithPopup(provider)
-        .then((result) => {
-            dispatch({
-                type: actionTypes.SET_USER,
-                user: result.user,
-            })
-        })
-        .catch((error) => alert(error.message));
-    };
-  
-  
-    return (
-    <div className='login'>
-        <div className='login__container'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6aerxbj8cZ1lICJKL4DX8m_HYy_mn9OavzA&usqp=CAU"
-            alt="" />
+  const [{}, dispatch] = useStateValue();
+  const signIn = (event) => {
+    event.preventDefault();
 
-            <div className='login__text'>
-            <h1>Sign in to Whatsapp</h1>
-            </div>
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
 
-            <Button onClick={signIn}>Sign In with Google</Button>
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6aerxbj8cZ1lICJKL4DX8m_HYy_mn9OavzA&usqp=CAU"
+          alt=""
+        />
+
+        <div className="login__text">
+          <h1>Sign in to Whatsapp</h1>
         </div>
+
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
